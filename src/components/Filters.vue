d2b9ad41350c387bc70989b9cc16b844e7105775
Paul Allen <me@paulallen.uk>
2022-06-05 22:48:56 +0100

    Removed Prettier formatting!

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,13 +1,6 @@
 <template>
-  <div
-    class="block md:max-w-screen-xl lg:max-w-screen-2xl md:mx-auto mb-12 md:mb-24 px-6 text-center"
-  >
-    <span
-      v-for="filter in filters"
-      :key="filter"
-      @click="() => filterTweets(filter)"
-      class="cursor-pointer text-slate-100 hover:text-blue-500 px-3"
-    >
+  <div class="block md:max-w-screen-xl lg:max-w-screen-2xl md:mx-auto mb-12 md:mb-24 px-6 text-center">
+    <span v-for="filter in filters" :key="filter" @click="() => filterTweets(filter)" class="cursor-pointer text-slate-100 hover:text-blue-500 px-3">
       {{ filter }}
     </span>
   </div>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,55 +1,25 @@
 <template>
   <section class="relative text-slate-400">
-    <div
-      class="relative z-10 py-24 md:py-32 lg:py-48 md:max-w-screen-xl md:mx-auto px-6"
-    >
+    <div class="relative z-10 py-24 md:py-32 lg:py-48 md:max-w-screen-xl md:mx-auto px-6">
       <div class="hero text-center text-slate-100 font-black tracking-tight">
-        <p
-          class="text-2xl mb-6 md:text-4xl lg:text-5xl lg:mb-12 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500"
-        >
+        <p class="text-2xl mb-6 md:text-4xl lg:text-5xl lg:mb-12 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500">
           TheOpenDAO $SOS
         </p>
-        <h2
-          class="py-2 text-4xl mb-12 md:text-7xl lg:text-8xl lg:mb-24 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500"
-        >
+        <h2 class="py-2 text-4xl mb-12 md:text-7xl lg:text-8xl lg:mb-24 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500">
           Probably Nothing, right?
         </h2>
-        <button
-          class="relative inline-flex items-center justify-center p-1 overflow-hidden text-lg text-slate-100 rounded group bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 group-hover:from-pink-500 group-hover:via-purple-500 group-hover:to-pink-500 hover:text-slate-200 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
-        >
-          <span
-            class="relative px-12 py-6 transition-all ease-in duration-75 dark:bg-black rounded group-hover:bg-opacity-0"
-          >
-            <a href="https://theopendao.com" target="_blank"
-              >Check out TheOpenDAO</a
-            >
+        <button class="relative inline-flex items-center justify-center p-1 overflow-hidden text-lg text-slate-100 rounded group bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 group-hover:from-pink-500 group-hover:via-purple-500 group-hover:to-pink-500 hover:text-slate-200 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span class="relative px-12 py-6 transition-all ease-in duration-75 dark:bg-black rounded group-hover:bg-opacity-0">
+            <a href="https://theopendao.com" target="_blank">Check out TheOpenDAO</a>
           </span>
         </button>
       </div>
     </div>
     <div class="absolute z-0 left-0 right-0 bottom-0 w-full">
-      <svg
-        class="w-full z-0"
-        viewBox="0 0 1920 798"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M1678 0L2323.19 787.199H1920V798H0V787.834H-255.372L143 383.752L298.091 541.066L677 200.771L980.326 473.186L1229 259.223L1367.72 378.578L1678 0Z"
-          fill="url(#paint0_linear_17_148)"
-          fill-opacity="0.10"
-        />
+      <svg class="w-full z-0" viewBox="0 0 1920 798" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M1678 0L2323.19 787.199H1920V798H0V787.834H-255.372L143 383.752L298.091 541.066L677 200.771L980.326 473.186L1229 259.223L1367.72 378.578L1678 0Z" fill="url(#paint0_linear_17_148)" fill-opacity="0.10" />
         <defs>
-          <linearGradient
-            id="paint0_linear_17_148"
-            x1="983"
-            y1="-635.35"
-            x2="979.77"
-            y2="797.997"
-            gradientUnits="userSpaceOnUse"
-          >
+          <linearGradient id="paint0_linear_17_148" x1="983" y1="-635.35" x2="979.77" y2="797.997" gradientUnits="userSpaceOnUse">
             <stop stop-color="#EC4899" />
             <stop offset="1" stop-color="#3B82F6" stop-opacity="0" />
           </linearGradient>
